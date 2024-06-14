7fc6ba49b847f0de170b58750a5b41a8b798b426
Jagan <jaganeditz@gmail.com>
2024-06-14 16:23:28 +0530

    - Add new component for Funder Includes add-funder,view funder & list funder

---
+++ src/app/core/components/admin/Funder/add-funder/add-funder.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-funder',
+  templateUrl: './add-funder.component.html',
+  styleUrl: './add-funder.component.css'
+})
+export class AddFunderComponent {
+
+}
---
+++ src/app/core/components/admin/Funder/funder-list/funder-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-funder-list',
+  templateUrl: './funder-list.component.html',
+  styleUrl: './funder-list.component.css'
+})
+export class FunderListComponent {
+
+}
