42759b983e0241fbb0cd95a82feb97eecc558d7c
Jagan <jaganeditz@gmail.com>
2024-06-14 17:24:32 +0530

    funder form updated

--- src/app/core/components/admin/Funder/add-funder/add-funder.component.ts
+++ src/app/core/components/admin/Funder/add-funder/add-funder.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-funder',
   templateUrl: './add-funder.component.html',
-  styleUrl: './add-funder.component.css'
+  styleUrl: './add-funder.component.css',
 })
 export class AddFunderComponent {
-
+  funderForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.funderForm = this.fb.group({
+      funderName: ['', Validators.required],
+      funderCatg: ['', Validators.required],
+      funderAdd1: ['', Validators.required],
+      funderAdd2: [''],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      state: ['', Validators.required],
+      pinCode: ['', [Validators.required, Validators.pattern('^[0-9]{6}$')]],
+      contactNumber: [
+        ,
+        [Validators.required, Validators.pattern('^[0-9]{10}$')],
+      ],
+      emailId: ['', [Validators.required, Validators.email]],
+      funderstatus: [200],
+    });
+  }
+  onSubmit(data: any) {
+    console.log(data);
+  }
 }
