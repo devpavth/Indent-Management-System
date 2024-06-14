124eb8487c519239cc7079b12cb3f2a41936ae99
Sharmila120501 <sharmilaanandhan1201@gmail.com>
2024-06-14 18:21:55 +0530

    funder view page create and funder error set

--- src/app/core/components/admin/Funder/funder-list/funder-list.component.ts
+++ src/app/core/components/admin/Funder/funder-list/funder-list.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-funder-list',
   templateUrl: './funder-list.component.html',
-  styleUrl: './funder-list.component.css'
+  styleUrl: './funder-list.component.css',
 })
 export class FunderListComponent {
-
+  isFunderList: Boolean = false;
+  closeView(close: Boolean) {
+    this.isFunderList = close;
+  }
 }
