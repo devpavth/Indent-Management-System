53d2eed01f01be2ae5535364e06f107ebde803b8
Sharmila120501 <sharmilaanandhan1201@gmail.com>
2024-06-15 13:18:28 +0530

    funder view fetch

--- src/app/core/components/admin/Funder/view-funder/view-funder.component.ts
+++ src/app/core/components/admin/Funder/view-funder/view-funder.component.ts
@@ -1,20 +1,32 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  SimpleChange,
+  SimpleChanges,
+} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FunderService } from '../../../service/Funder/funder.service';
 
 @Component({
   selector: 'app-view-funder',
   templateUrl: './view-funder.component.html',
   styleUrl: './view-funder.component.css',
 })
-export class ViewFunderComponent {
-  @Output() closeFunderPop = new EventEmitter<boolean>();
-
+export class ViewFunderComponent implements OnInit {
+  @Output() closeFunderView = new EventEmitter<boolean>();
+  @Input() funderData: any;
   isSave = false;
   isEdit = true;
   isSaveIcon = true;
   isDelete = false;
   updateFunderForm: FormGroup;
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private funderService: FunderService,
+  ) {
     this.updateFunderForm = this.fb.group({
       funderName: ['', Validators.required],
       funderCatg: ['', Validators.required],
@@ -32,9 +44,13 @@ export class ViewFunderComponent {
       funderstatus: [200],
     });
   }
-  closeFunderView() {
-    this.closeFunderPop.emit(false);
+  ngOnInit(): void {
+    this.updateFunderForm.patchValue(this.funderData);
+    Object.keys(this.updateFunderForm.controls).forEach((form) => {
+      this.updateFunderForm.get(form)?.disable();
+    });
   }
+
   edit() {
     Object.keys(this.updateFunderForm.controls).forEach((form) => {
       this.updateFunderForm.get(form)?.enable();
