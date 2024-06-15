8f828b80bba863af7d0a0a9f20fc87e0ef5f6c6e
Jagan <jaganeditz@gmail.com>
2024-06-15 12:48:33 +0530

    funder list updated

--- src/app/core/components/admin/Funder/funder-list/funder-list.component.ts
+++ src/app/core/components/admin/Funder/funder-list/funder-list.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FunderService } from '../../../service/Funder/funder.service';
 
 @Component({
   selector: 'app-funder-list',
   templateUrl: './funder-list.component.html',
   styleUrl: './funder-list.component.css',
 })
-export class FunderListComponent {
+export class FunderListComponent implements OnInit {
   isFunderList: Boolean = false;
-  closeView(close: Boolean) {
-    this.isFunderList = close;
+  funderData: any;
+
+  funderList: any;
+  constructor(private funderService: FunderService) {}
+  ngOnInit() {
+    this.getAllFunderList();
+  }
+
+  getAllFunderList() {
+    this.funderService.funderList().subscribe((res) => {
+      this.funderList = res;
+      console.log(res);
+    });
+  }
+
+  toggleView(action: Boolean, check: number, funderData: any) {
+    if (check == 1) {
+      this.isFunderList = action;
+      this.funderData = funderData;
+    }
+    if (check == 0) {
+      this.isFunderList = action;
+    }
   }
 }
