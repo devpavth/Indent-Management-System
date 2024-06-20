4d1fd4d8da5d582a5b733c8cb2a4036a05bab34b
Sharmila120501 <sharmilaanandhan1201@gmail.com>
2024-06-20 17:42:15 +0530

    vendor add, get,delete,update finish

--- src/app/core/components/admin/vendor/view-vendor/view-vendor.component.ts
+++ src/app/core/components/admin/vendor/view-vendor/view-vendor.component.ts
@@ -1,10 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { VendorService } from '../../../service/vendor/vendor.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-view-vendor',
   templateUrl: './view-vendor.component.html',
-  styleUrl: './view-vendor.component.css'
+  styleUrl: './view-vendor.component.css',
 })
 export class ViewVendorComponent {
+  isChecked: boolean = false;
+  @Output() closeVendor = new EventEmitter<boolean>();
+  @Input() vendorData: any;
+  isSave = false;
+  isEdit = true;
+  isSaveIcon = true;
+  deleteVendor: any;
+  isDelete: boolean = false;
+  UpdateVendorForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private vendorService: VendorService,
+    private route: Router,
+  ) {
+    this.UpdateVendorForm = this.fb.group({
+      vendorId: [],
+      vendorName: [],
+      vdrAdd1: [],
+      vdrAdd2: [],
+      vdrCity: [],
+      vdrState: [],
+      vdrCountry: [],
+      vdrPincode: [],
+      vdrContactPersonName: [],
+      vdrContactPersonPhone: [],
+      vdrEmail: [],
+      vdrGstNo: [],
+      vdrPanNo: [],
+      vdrTanNo: [],
+      vdrMsmeNo: [],
+      estDate: [],
+      serviceLocation: [],
+      bizType: [],
+      bizDetailName: [],
+      bizDetails: [],
+      ifsCode: [],
+      bankAccNo: [],
+    });
+  }
 
+  ngOnInit(): void {
+    this.UpdateVendorForm.patchValue(this.vendorData);
+    Object.keys(this.UpdateVendorForm.controls).forEach((form) => {
+      this.UpdateVendorForm.get(form)?.disable();
+    });
+  }
+  edit() {
+    Object.keys(this.UpdateVendorForm.controls).forEach((form) => {
+      this.UpdateVendorForm.get(form)?.enable();
+    });
+    this.isSave = true;
+    this.isEdit = false;
+  }
+  updateVendorDetails(data: any) {
+    console.log(data);
+    let id = this.UpdateVendorForm.get('vendorId')?.value;
+    console.log(id);
+
+    this.vendorService.updateVendor(id, data).subscribe((res) => {
+      console.log(res);
+    });
+  }
+
+  toggledelete(check: any, isView: boolean) {
+    if (check == 1) {
+      this.isDelete = isView;
+      this.deleteVendor = {
+        title: 'Vendor',
+        action: 3,
+        deleteId: this.vendorData.vendorId,
+      };
+    } else if (check == 0) {
+      this.isDelete = isView;
+      this.closeVendor.emit(false);
+    }
+  }
 }
--- src/app/shared/delete/delete.component.ts
+++ src/app/shared/delete/delete.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FunderService } from '../../core/components/service/Funder/funder.service';
 import { ProductService } from '../../core/components/service/Product/product.service';
+import { VendorService } from '../../core/components/service/vendor/vendor.service';
 
 @Component({
   selector: 'app-delete',
@@ -14,6 +15,7 @@ export class DeleteComponent {
   constructor(
     private funderService: FunderService,
     private productService: ProductService,
+    private vendorService: VendorService,
   ) {}
 
   deleteFunction() {
@@ -35,6 +37,19 @@ export class DeleteComponent {
     if (this.deleteData.action == 2) {
       console.log(this.deleteData);
       this.productService.deleteProduct(this.deleteData.deleteId).subscribe(
+        (res) => {
+          console.log(res);
+        },
+        (error) => {
+          if (error.status == 200) {
+            this.close.emit(false);
+          }
+        },
+      );
+    }
+    if (this.deleteData.action == 3) {
+      console.log(this.deleteData);
+      this.vendorService.deleteVendor(this.deleteData.deleteId).subscribe(
         (res) => {
           console.log(res);
         },
