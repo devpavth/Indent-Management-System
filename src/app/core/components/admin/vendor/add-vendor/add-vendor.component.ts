818f6f9434f06eeac2400ea595540e066cd4890d
Sharmila120501 <sharmilaanandhan1201@gmail.com>
2024-06-21 10:19:01 +0530

    vendor validatation finish

--- src/app/core/components/admin/vendor/add-vendor/add-vendor.component.ts
+++ src/app/core/components/admin/vendor/add-vendor/add-vendor.component.ts
@@ -17,27 +17,73 @@ export class AddVendorComponent {
     private route: Router,
   ) {
     this.addVendorForm = this.fb.group({
-      vendorName: [],
-      vdrAdd1: [],
-      vdrAdd2: [],
-      vdrCity: [],
-      vdrState: [],
-      vdrCountry: [],
-      vdrPincode: [],
-      vdrContactPersonName: [],
-      vdrContactPersonPhone: [],
-      vdrEmail: [],
-      vdrGstNo: [],
-      vdrPanNo: [],
-      vdrTanNo: [],
+      vendorName: ['', [Validators.required, Validators.pattern('[A-Za-z ]+')]],
+      vdrAdd1: ['', Validators.required],
+      vdrAdd2: ['', Validators.required],
+      vdrCity: ['', Validators.required],
+      vdrState: ['', Validators.required],
+      vdrCountry: ['', Validators.required],
+      vdrPincode: [
+        '',
+        [Validators.required, Validators.pattern(/^[1-9][0-9]{5}$/)],
+      ],
+      vdrContactPersonName: [
+        '',
+        [Validators.required, Validators.pattern('[A-Za-z ]+')],
+      ],
+      vdrContactPersonPhone: [
+        '',
+        [Validators.required, Validators.pattern(/^[1-9][0-9]{9}$/)],
+      ],
+      vdrEmail: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^[a-zA-Z0-9._%+-]+@[a-z]+.([a-z]{2})+(?:\.(com|in|edu|net)){1}$/,
+          ),
+        ],
+      ],
+      vdrGstNo: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^\d{2}[A-Z]{5}\d{4}[A-Z]{1}\d{1}[A-Z\d]{2}$/),
+        ],
+      ],
+      vdrPanNo: [
+        '',
+        [Validators.required, Validators.pattern(/^[A-Z]{5}[0-9]{4}[A-Z]{1}$/)],
+      ],
+      vdrTanNo: [
+        '',
+        [Validators.required, Validators.pattern(/^[A-Z]{4}[0-9]{5}[A-Z]$/)],
+      ],
       vdrMsmeNo: [],
-      estDate: [],
-      serviceLocation: [],
-      bizType: [],
-      bizDetailName: [],
-      bizDetails: [],
-      ifsCode: [],
-      bankAccNo: [],
+      estDate: ['', Validators.required],
+      serviceLocation: ['', Validators.required],
+      bizType: ['', Validators.required],
+      bizDetailName: [
+        '',
+        [Validators.required, Validators.pattern('[A-Za-z ]+')],
+      ],
+      bizDetails: ['', Validators.required],
+      ifsCode: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^[A-Za-z]{4}0[A-Za-z0-9]{6}$/),
+        ],
+      ],
+      bankAccNo: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^-?\d*\.?\d+$/),
+          Validators.minLength(8),
+          Validators.maxLength(15),
+        ],
+      ],
     });
   }
 
--- src/app/core/components/admin/vendor/view-vendor/view-vendor.component.ts
+++ src/app/core/components/admin/vendor/view-vendor/view-vendor.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { VendorService } from '../../../service/vendor/vendor.service';
 import { Router } from '@angular/router';
 
@@ -25,27 +25,73 @@ export class ViewVendorComponent {
   ) {
     this.UpdateVendorForm = this.fb.group({
       vendorId: [],
-      vendorName: [],
-      vdrAdd1: [],
-      vdrAdd2: [],
-      vdrCity: [],
-      vdrState: [],
-      vdrCountry: [],
-      vdrPincode: [],
-      vdrContactPersonName: [],
-      vdrContactPersonPhone: [],
-      vdrEmail: [],
-      vdrGstNo: [],
-      vdrPanNo: [],
-      vdrTanNo: [],
+      vendorName: ['', [Validators.required, Validators.pattern('[A-Za-z ]+')]],
+      vdrAdd1: ['', Validators.required],
+      vdrAdd2: ['', Validators.required],
+      vdrCity: ['', Validators.required],
+      vdrState: ['', Validators.required],
+      vdrCountry: ['', Validators.required],
+      vdrPincode: [
+        '',
+        [Validators.required, Validators.pattern(/^[1-9][0-9]{5}$/)],
+      ],
+      vdrContactPersonName: [
+        '',
+        [Validators.required, Validators.pattern('[A-Za-z ]+')],
+      ],
+      vdrContactPersonPhone: [
+        '',
+        [Validators.required, Validators.pattern(/^[1-9][0-9]{9}$/)],
+      ],
+      vdrEmail: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^[a-zA-Z0-9._%+-]+@[a-z]+.([a-z]{2})+(?:\.(com|in|edu|net)){1}$/,
+          ),
+        ],
+      ],
+      vdrGstNo: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^\d{2}[A-Z]{5}\d{4}[A-Z]{1}\d{1}[A-Z\d]{2}$/),
+        ],
+      ],
+      vdrPanNo: [
+        '',
+        [Validators.required, Validators.pattern(/^[A-Z]{5}[0-9]{4}[A-Z]{1}$/)],
+      ],
+      vdrTanNo: [
+        '',
+        [Validators.required, Validators.pattern(/^[A-Z]{4}[0-9]{5}[A-Z]$/)],
+      ],
       vdrMsmeNo: [],
-      estDate: [],
-      serviceLocation: [],
-      bizType: [],
-      bizDetailName: [],
-      bizDetails: [],
-      ifsCode: [],
-      bankAccNo: [],
+      estDate: ['', Validators.required],
+      serviceLocation: ['', Validators.required],
+      bizType: ['', Validators.required],
+      bizDetailName: [
+        '',
+        [Validators.required, Validators.pattern('[A-Za-z ]+')],
+      ],
+      bizDetails: ['', Validators.required],
+      ifsCode: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^[A-Za-z]{4}0[A-Za-z0-9]{6}$/),
+        ],
+      ],
+      bankAccNo: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^-?\d*\.?\d+$/),
+          Validators.minLength(8),
+          Validators.maxLength(15),
+        ],
+      ],
     });
   }
 
