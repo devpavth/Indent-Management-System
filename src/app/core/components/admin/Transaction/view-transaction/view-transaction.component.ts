c1c3fc92c483f906fefc7de16be440b3d0934f70
Pavithradevi <paviguna116@gmail.com>
2025-01-09 20:04:13 +0530

    worked on product transction page to easily to view their transaction and the user can confirm they transaction by accept button

--- src/app/core/components/admin/Transaction/view-transaction/view-transaction.component.ts
+++ src/app/core/components/admin/Transaction/view-transaction/view-transaction.component.ts
@@ -5,6 +5,7 @@ import { debounceTime, switchMap, of, catchError } from 'rxjs';
 import { BranchService } from '../../../service/Branch/branch.service';
 import { SharedServiceService } from '../../../service/shared-service/shared-service.service';
 import { VendorService } from '../../../service/vendor/vendor.service';
+import { ProductService } from '../../../service/Product/product.service';
 
 @Component({
   selector: 'app-view-transaction',
@@ -15,8 +16,8 @@ export class ViewTransactionComponent {
   isChecked: boolean = false;
   @Output() closeVendor = new EventEmitter<boolean>();
   @Input() transactionData: any;
-  isSave = false;
-  isEdit = true;
+  isSave = true;
+  // isEdit = true;
   isSaveIcon = true;
   deleteVendor: any;
   isDelete: boolean = false;
@@ -25,6 +26,10 @@ export class ViewTransactionComponent {
   noPincode: boolean = false;
   pincodeList: any[] = [];
   cityDropDownOptions: any;
+
+  transactionList: any;
+
+  private productService = inject(ProductService);
 
   private sharedService = inject(SharedServiceService);
 
@@ -37,49 +42,16 @@ export class ViewTransactionComponent {
     private route: Router,
   ) {
     this.TransactionForm = this.fb.group({
-      prdtransId: [],
       prdtrnsCode: [],
-      transType: ['', [Validators.required, Validators.pattern('[A-Za-z ]+')]],
-      tranRefNo: ['', Validators.required],
-      totalPrice: ['', Validators.required],
-      tranUpdBy: ['', Validators.required],
-      tranUpdOn: ['', Validators.required],
-      prdUnit: ['', Validators.required],
-      totalPieces: [
-        '',
-        [Validators.required],
-      ],
-      prdQty: [
-        '',
-        [Validators.required],
-      ],
-      purchasedPrice: [
-        '',
-        [Validators.required],
-      ],
-      gstPercentage: [
-        '',
-        [
-          Validators.required
-        ],
-      ],
-      itemTotalPrice: [
-        '',
-        [
-          Validators.required
-        ],
-      ],
-      updatedBy: [
-        '',
-        [Validators.required],
-      ],
-      updateOn: [
-        '',
-        [Validators.required],
-      ],
+      inwardFromCode: [],
+      transType: [],
+      tranRefNo: [],
+      totalPrice: [],
+      tranUpdBy: [],
+      tranUpdOn: [],
+      
       branchDto: this.fb.group({
         branchName: [],
-        branchCode: [],
         add1: [],
         add2: [],
         city: [],
@@ -90,7 +62,15 @@ export class ViewTransactionComponent {
         branchMobilenumber: []
       }),
       vendorDto: this.fb.group({
-
+        branchName: [],
+        add1: [],
+        add2: [],
+        city: [],
+        state: [],
+        pinCode: [],
+        gstNumber: [],
+        manager: [],
+        branchMobilenumber: []
       }),
       transPrdDetails: this.fb.array([this.showTransactionDetails()]),
     });
@@ -98,132 +78,63 @@ export class ViewTransactionComponent {
 
   showTransactionDetails(){
     return this.fb.group({
-      prdmdlName: [],
-      prdDescription: [],
-      prdCode: [],
-      prdHsnCode: [],
-      prdPurchasedPrice: [],
+      transProductData: this.fb.group({
+        prdmdlName: [],
+        prdDescription: [],
+        prdCode: [],
+        prdHsnCode: [],
+        prdbrndName: [],
+        prdgrpName: [],
+        prdcatgName: [],
+        headOfAccName: []
+      }),
       prdUnit: [],
-      prdGstPct: [],
-      prdMinQty: [],
-      prdClosingStock: [],
-      prdTotalValue: [],
-      createdBy: [],
-      createdOn: [],
-      prdbrndName: [],
-      prdgrpName: [],
-      prdcatgName: [],
-      headOfAccName: []
+      totalPieces: [],
+      prdQty: [],
+      purchasedPrice: [],
+      gstPercentage: [],
+      itemTotalPrice: [],
+      updatedBy: [],
+      updateOn: [],
     })
   }
 
   ngOnInit(): void {
-    this.getBranchName();
-    this.TransactionForm.patchValue(this.transactionData);
-    console.log("this.vendorData:", this.transactionData);
-    console.log("this.vendorData:", this.transactionData.vdrCity);
+    // this.getBranchName();
+    this.TransactionForm.patchValue(this.transactionData);  
+    console.log("this.transactionData:", this.transactionData);
     Object.keys(this.TransactionForm.controls).forEach((form) => {
       this.TransactionForm.get(form)?.disable();
     });
 
-
-    // this.UpdateVendorForm.patchValue({
-    //   vdrCity: this.vendorData.vdrCity
-    // })
-
-    this.TransactionForm.get('vdrPincode')?.valueChanges
-    .pipe(
-      debounceTime(300),
-      switchMap((pincode) => {
-        if(this.isPincodeSelected){
-          return of([]);
-        }
-        this.noPincode = false;
-        if(!pincode || pincode.toString().length !==6){
-          this.pincodeList = [];
-          return of([]);
-        }
-        return this.sharedService.fetchPincode(pincode).pipe(
-          catchError((error)=>{
-            if(error.status === "Error"){
-              console.log("Pincode API Error:", error);
-              this.noPincode = true;
-            }
-            return of([]);
-          })
-        )
-      })
-    )
-    .subscribe(
-      (response: any) => {
-        const postOfficeArray = response?.[0]?.postOffice ?? [];
-        console.log("postOfficeArray:", postOfficeArray);
-
-        this.pincodeList = postOfficeArray;
-        console.log("reponse from pincode:", response);
-        console.log("typeof Pincode API Error:", typeof response?.[0]?.status);
-
-        if(response?.[0]?.status === "Error"
-          && this.pincodeList.length === 0
-        ){
-          console.log("Pincode API Error:", response?.[0]?.status);
-          this.noPincode = true;
-
-          this.TransactionForm.patchValue({
-            vdrCity: '',
-            vdrState: '',
-            vdrCountry: ''
-          },{emitEvent: false})
-        }
-        console.log("fetching pincode with live search:", this.pincodeList);
-
-        if(this.pincodeList.length > 0){
-          const cityDropDownOptions = this.pincodeList.map(
-            (address) => ({
-              label: `${address.name}, ${address.city}`,
-              value: `${address.name}, ${address.city}`
-            })
-          )
-
-          console.log("cityDropDownOptions:", cityDropDownOptions);
-
-          this.cityDropDownOptions = cityDropDownOptions;
-
-          this.TransactionForm.patchValue({
-            vdrCity: cityDropDownOptions[0].value,
-            vdrState: this.pincodeList[0].state,
-            vdrCountry: this.pincodeList[0].country
-          },
-        {emitEvent: false}
-      )
-        
-        }
-        this.isPincodeSelected = false;
+    this.productService.productTransaction(this.transactionData.prdtrnsCode).subscribe(
+      (res) =>{
+        console.log("fetching transaction details:", res);
+        this.transactionList = res;
+      },
+      (error) => {
+        console.log("error while fetching transaction details:", error);
       }
     )
+
   }
 
-  get vendorAcccountDetails() {
-    return this.TransactionForm.get('vendorAcccountDetails') as FormArray;
+  get transactionDetails() {
+    return this.TransactionForm.get('transPrdDetails') as FormArray;
   }
 
-  showBankData() {
-    return this.fb.group({
-      ifsCode: ['', Validators.required],
-      bankAccNo: ['', Validators.required],
-    });
-  }
-
-  addbank() {
-    this.vendorAcccountDetails.push(this.showBankData());
-  }
   edit() {
     Object.keys(this.TransactionForm.controls).forEach((form) => {
       this.TransactionForm.get(form)?.enable();
     });
     this.isSave = true;
-    this.isEdit = false;
+    // this.isEdit = false;
   }
+
+  acceptTransactionDetails(code: any){
+    
+  }
+
   updateVendorDetails(data: any) {
     console.log(data);
     let id = this.TransactionForm.get('vendorId')?.value;
@@ -241,13 +152,13 @@ export class ViewTransactionComponent {
       },
     );
   }
-  getBranchName() {
-    this.branchService.getBranch().subscribe((res) => {
-      console.log(res);
-      this._BranchName = res;
-      console.log(this._BranchName);
-    });
-  }
+  // getBranchName() {
+  //   this.branchService.getBranch().subscribe((res) => {
+  //     console.log(res);
+  //     this._BranchName = res;
+  //     console.log(this._BranchName);
+  //   });
+  // }
 
 
 }
